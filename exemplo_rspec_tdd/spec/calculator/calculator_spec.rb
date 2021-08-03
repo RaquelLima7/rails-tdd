87af45c2a6d3eba9ba8736af851564f3ad2290ba
RaquelLima7 <raquellimadev@gmail.com>
2021-08-02 22:30:11 -0300

    lecture 2 - subject, xit ...

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,15 +1,22 @@
 require 'calculator' #do a require where the file is
 
 describe Calculator do #describe because it will describe about the element which in this case is the Calculator
-  it 'sum method for two numbers' do
-    calc = Calculator.new  #preparation
-    result = calc.sum(5, 7) #exercise
-    expect(result).to eql(12)  #what do you expect
-  end
 
-  it 'use sum method for 2 numbers with negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(-5, 7)
-    expect(result).to eql(2)
+  subject(:calc) { described_class.new() } #subject é a classe Calculator
+  context '#sum' do # context para poder agrupar os tipos de testes #sum referring to an instance method's name
+    it 'positive numbers' do
+      result = calc.sum(5, 7) #exercise  
+      expect(result).to eql(12)  #what do you expect
+    end
+
+    it 'negative and positive numbers' do
+      result = calc.sum(-5, 7)
+      expect(result).to eql(2)
+    end
+
+    xit 'negative numbers' do #acrescenta o x na frente para deixar o teste pendente
+      result = calc.sum(-5, -7)
+      expect(result).to eql(-12)
+    end
   end
 end

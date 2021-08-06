9614ae5246794f945e8f2a7b7a695953606948fd
RaquelLima7 <raquellimadev@gmail.com>
2021-08-05 22:52:38 -0300

    Hooks (before e after)

--- exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
@@ -10,27 +10,29 @@ describe 'Atributos' do
       puts ">>>>>>>>>> DEPOIS de TODOS os testes"
     end
 
-   around(:each) do |teste|
-    puts "ANTES"
-    @pessoa = Pessoa.new
+   around(:each) do |teste| # 1-antes de cada teste, instancia uma nova pessoa 
+    puts "ANTES" 
+    @pessoa = Pessoa.new   # 6-depois que imprimir o nome vai e instancia uma nova pessoa 
 
     teste.run # roda o teste
 
-    @pessoa.nome = "Sem nome!"
-    puts "DEPOIS >>>>>>> #{@pessoa.inspect}"
+    @pessoa.nome = "Sem nome!"  # 4-depois que fizer o teste, volta e coloca sem nome
+                                # 9 - depois muda para sem nome de novo
+    puts "DEPOIS >>>>>>> #{@pessoa.inspect}"  # 5-imprimi para ver se realmente ficou sem nome
+                                              # 10-e mostra
    end
 
-   it 'have_attributes' do
+   it 'have_attributes' do    # 2-preenche os dados
      @pessoa.nome = "Jackson"
      @pessoa.idade = 20
 
-     expect(@pessoa).to have_attributes(nome: a_string_starting_with("J"), idade: (a_value >= 20))
+     expect(@pessoa).to have_attributes(nome: a_string_starting_with("J"), idade: (a_value >= 20))  #3-faz o teste
    end
 
    it 'have_attributes' do
-     @pessoa.nome = "Jose"
+     @pessoa.nome = "Jose"  # 7-add os valores
      @pessoa.idade = 25
 
-     expect(@pessoa).to have_attributes(nome: a_string_starting_with("J"), idade: (a_value >= 20))
+     expect(@pessoa).to have_attributes(nome: a_string_starting_with("J"), idade: (a_value >= 20)) # 8-faz os testes
    end
 end

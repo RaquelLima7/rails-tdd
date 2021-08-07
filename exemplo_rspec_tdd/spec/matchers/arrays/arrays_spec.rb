32c339d40d62d6f8bf3cc3491bf1378442686760
RaquelLima7 <raquellimadev@gmail.com>
2021-08-06 22:04:42 -0300

    Negativando Matchers

--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -6,7 +6,7 @@ describe Array.new([1,2,3]), "Array", type: 'collection' do
     expect(subject).to include(2,1) # elementos para verificar se include
   end
 
-  it { expect(subject).to exclude(4) }
+  it { expect(subject).to exclude(4) } #nesse caso é para excluir o 4
 
   it '#contain_exactly', :slow do
     expect(subject).to contain_exactly(2,1,3) # se inclui tais elementos, pode colocar em ordens diferentes

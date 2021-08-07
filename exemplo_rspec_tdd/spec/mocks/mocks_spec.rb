bbdfbeb7aa563823fd57c05e9e7edd2a69457236
RaquelLima7 <raquellimadev@gmail.com>
2021-08-07 15:11:16 -0300

    Mock Expectations

--- exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
+++ exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
@@ -21,14 +21,14 @@ describe 'Mocks' do
 
   it 'repetição' do
     student = Student.new
-    expect(student).to receive(:foo).with(123).twice
-    student.foo(123)
+    expect(student).to receive(:foo).with(123).twice #contagem, pode dizer quantas vezes quer que receba
+    student.foo(123) #esses dois cumprem com a linha acima, pois está chamando o elemento duas vezes
     student.foo(123)
   end
 
   it 'retorno' do
     student = Student.new
-    expect(student).to receive(:foo).with(123).and_return(true)
-    puts student.foo(123)
+    expect(student).to receive(:foo).with(123).and_return(true) # esse (true) é fake, pois está dizendo o valor que espera
+    puts student.foo(123)   #imprimi isso para poder ver, está forçando o retorno dele, o parâmetro que está esperando (123)
   end
 end
